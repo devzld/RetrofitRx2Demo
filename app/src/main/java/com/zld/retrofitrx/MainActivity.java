937c95bf0e03e1f3186603dc16a98509e7e1845b
lingdong <779724606@qq.com>
2017-12-06 14:38:51 +0800

    RxLifecycle使用

--- app/src/main/java/com/zld/retrofitrx/MainActivity.java
+++ app/src/main/java/com/zld/retrofitrx/MainActivity.java
@@ -1,10 +1,10 @@
 package com.zld.retrofitrx;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
 import com.zld.retrofitrx.api.ToutiaoApi;
 import com.zld.retrofitrx.bean.NewsBean;
 import com.zld.retrofitrx.utils.RetrofitFactory;
@@ -13,7 +13,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends RxAppCompatActivity {
 
     TextView tv;
 
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
                 .getToutiaoNews("top", "44989df28ef48c89d700d68c10c2c74c")
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
+                .compose(this.<NewsBean>bindToLifecycle())
                 .subscribe(new Consumer<NewsBean>() {
                     @Override
                     public void accept(NewsBean newsBean) throws Exception {
